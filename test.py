a bunch of changes.  We can now average many density matrices and "smear" density matrices over time.  Several important bugs were also fixed.

test.py
@@ -2,7 +2,11 @@ import numpy as np
 import math
 import incoher.dm as dm
 
-nk = 10
+np.random.seed(25892)
+
+nruns = 1000
+
+nk = 2
 
 omega = .01
 
@@ -12,34 +16,30 @@ k = mass * omega * omega
 
 deltae = 0.20
 
-c = -0.2
+b = -0.2
+#b = 0.0
 
 dt = 1.0
 
-intensity = 1.0
+intensity = 1.0e-3
 
-rho = dm(nk,k,deltae,c,mass,dt,intensity)
+rho = dm(nk,k,deltae,b,mass,dt,intensity)
 
-rho.init_0k()
+rhobar = dm(nk,k,deltae,b,mass,dt,intensity)
 
-print rho.rho00r
-print rho.rho00i
-print rho.omega
+rhobar.open_output_file("rhobar.hdf5")
 
-rho.build_H()
-rho.build_mu()
+for irun in range(nruns):
 
-print rho.H00
-print rho.H11
-print rho.mu01
-print rho.mu10
+    rho.open_output_file("rho.hdf5")
 
-rho.init_0k_excited()
+    rho.init_0k()
 
-rho.compute_rhodot_r_H()
-rho.compute_rhodot_i_H()
+    rho.prop_whitenoise(10000,100)
 
-print rho.rhodot00r_H
-print rho.rhodot00i_H
-print rho.rhodot11r_H
-print rho.rhodot11i_H
+    rhobar.add_dm_to_average(rho)
+
+    rho.close_output_file()
+
+rhobar.close_output_file()
+